8faf221c6f76aa55901797be6cb25544684f8ff1
Igor Oganesian <igor.oganesian@gmail.com>
2023-12-03 15:05:01 -0500

    post route

--- routes/lessonRoutes.ts
+++ routes/lessonRoutes.ts
@@ -1,15 +1,43 @@
 import express from 'express';
+import pool from '../db';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    // const { rows } = await pool.query('SELECT * FROM lessons');
-    // res.json(rows);
+    const { rows } = await pool.query('SELECT * FROM lessons');
+    res.json(rows);
     res.json({ message: 'This is the lessons endpoint'});
   } catch (err) {
     res.status(500).send('Server error');
   }
 });
+
+router.post('/', async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res
+            .status(400)
+            .json({ message: 'Title and content are required.' });
+  }
+
+  try {
+    const insertQuery = 'INSERT INTO lessons (title, content) VALUES ($1, $2) RETURNING *';
+    const values = [title, content];
+
+    const { rows } = await pool.query(insertQuery, values);
+
+    res.status(201).json(rows[0]);
+  } catch (err) {
+    if (typeof (err as { message: unknown }).message === 'string') {
+      console.error('Error:', (err as { message: string }).message);
+      res.status(500).json({ message: (err as { message: string }).message});
+    } else {
+      console.error('Unknown Error:', err);
+      res.status(500).json({ message: 'An unknown error occured.' });
+    }
+  }
+})
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import lessonRoutes from '../routes/lessonRoutes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/lessons', lessonRoutes);
 
 app.get('/', (req, res) => {
